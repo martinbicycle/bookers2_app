5261c94411505f34796894dd9533e175262f151e
martinbicycle <martinbicycle7700@gmail.com>
2020-12-11 15:54:31 +0000

    [Add] ユーザーの編集画面作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = User.all
+  end
+
+  def show
+    @book = Book.new
+    @books = Book.all
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '773facce8c5a040a86b73726680a2b8df1c3396105a35bcbecd71b0afbbe4d4a14d7438adc5333e76e04f1ae49442891bad0d5b9224ceb6a73bce996df14c0d1'
