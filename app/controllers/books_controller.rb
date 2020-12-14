a0124947a5798da6939779258c425aafa1419cdc
martinbicycle <martinbicycle7700@gmail.com>
2020-12-14 16:12:37 +0000

    [Add] サクセスメッセージの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,17 @@ class BooksController < ApplicationController
       @book = Book.new(book_params)
       @book.user_id = current_user.id
     if  @book.save
-        redirect_to book_path(@book)
+        redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
+        @books = Book.all
       　render :index
     end
-
   end
-
-
 
   def index
     @book = Book.new
     @books = Book.all
+    @book.user_id = current_user.id
     @user = current_user
   end
 
@@ -42,11 +41,10 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to books_path(@book), notice: 'Book was successfully destroyed.'
   end
 
   private
-
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  
+
   def index
     @book = Book.new
     @books = Book.all
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
   end
 
   private
