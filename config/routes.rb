1c885054d536fbe121fe744434e6d34b3d09b51d
martinbicycle <martinbicycle7700@gmail.com>
2020-12-16 14:18:45 +0000

    [Add] RSpecエラー修正中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'homes#index'
-  get "homes/about" => "homes#about"
+  root to: 'home#index'
+  get "home/about" => "home#about"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
 end
