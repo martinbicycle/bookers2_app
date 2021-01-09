525ab55c6940230c5dcd79b7a8585ab267a840f6
martinbicycle <martinbicycle7700@gmail.com>
2021-01-09 14:36:35 +0000

    [Add] relationshipsコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
-人
   has_many :followings, through: :relationships, source: :followed
 
   def follow(user_id)
